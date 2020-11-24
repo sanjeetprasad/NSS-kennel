45ea6e1e4ab56d242bdc523d2f88e488de0d50cb
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2020-11-23 21:12:46 -0600

    populating the navbar with routing, created applicationVies.js, navbar.js and css file,

--- src/components/animal/Animal.js
+++ src/components/animal/Animal.js
@@ -5,7 +5,7 @@ export const Animal = ({animal}) => (
     <section className="animal">
         <h3 className="animal__name">{animal.name}</h3>
         <div className="animal__breed">{animal.breed}</div>
-        <div className="animal__customerId">CustomerId: {animal.customerId}</div>
-        <div className="animal__locationId">LocationId: {animal.locationId}</div>
+        <div className="animal__customerId">Customer: {animal.customerId}</div>
+        <div className="animal__locationId">Location: {animal.locationId}</div>
     </section>
 )
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from "react-router-dom"
 import './index.css';
 import {Kennel} from "./components/Kennel"
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Kennel />
+    <Router>
+        <Kennel />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
