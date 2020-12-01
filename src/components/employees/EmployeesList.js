1fa48aeb12b2ae8ab5f41be7ea5166c738c222e1
Sanjeet Prasad <sanjeet.l.prasad@gmail.com>
2020-12-01 11:53:43 -0600

    created the employeeDetail.js to use the useContext, useState, and useEffect, added a new dynamic route for the employeeId (\d+) and a link in the employeeList to get the employee id and the name.

--- src/components/employees/EmployeesList.js
+++ src/components/employees/EmployeesList.js
@@ -1,24 +1,33 @@
 import React, { useContext, useEffect } from "react"
 import { EmployeeContext } from "./EmployeesProvider"
-import { Employee } from "./Employees"
+import { Link } from "react-router-dom"
+// import { Employee } from "./Employees"
 import "./Employees.css"
-import { LocationContext } from "../location/LocationProvider"
+// import { LocationContext } from "../location/LocationProvider"
 
-export const EmployeeList = (props) => {
-    // This state changes when `getLocations()` is invoked below
+
+export const EmployeeList = props => {
     const { employees, getEmployees } = useContext(EmployeeContext)
-    const {locations, getLocations } = useContext(LocationContext)
 
-    /*
-        What's the effect this is reponding to? Component was
-        "mounted" to the DOM. React renders blank HTML first,
-        then gets the data, then re-renders.
-    */
     useEffect(() => {
-        console.log("EmployeeList: Initial render before data")
-        getLocations()
-        .then(getEmployees)
+        getEmployees()
     }, [])
+
+// export const EmployeeList = (props) => {
+//     // This state changes when `getLocations()` is invoked below
+//     const { employees, getEmployees } = useContext(EmployeeContext)
+//     const {locations, getLocations } = useContext(LocationContext)
+
+//     /*
+//         What's the effect this is reponding to? Component was
+//         "mounted" to the DOM. React renders blank HTML first,
+//         then gets the data, then re-renders.
+//     */
+//     useEffect(() => {
+//         console.log("EmployeeList: Initial render before data")
+//         getLocations()
+//         .then(getEmployees)
+//     }, [])
 
     /*
         This effect is solely for learning purposes. The effect
@@ -39,19 +48,39 @@ export const EmployeeList = (props) => {
 // }
 
 
+// return (
+//     <div className="employees">
+//         <h1>Employees</h1>
+//         <button className= "add-employee" onClick={() => props.history.push("/employees/create")}>
+//             Add Employee
+//         </button>
+//         <article className="employeeList">
+//             {employees.map(employee => {
+//             const clinic = locations.find(loc => loc.id === employee.locationId)
+            
+//             return <Employee key={employee.id} employee={employee} location={clinic} />})}
+//         </article>
+            
+//     </div>
+
+
 return (
     <div className="employees">
         <h1>Employees</h1>
-        <button className= "add-employee" onClick={() => props.history.push("/employees/create")}>
+
+        <button onClick={() => props.history.push("/employees/create")}>
             Add Employee
         </button>
-        <article className="employeeList">
-            {employees.map(employee => {
-            const clinic = locations.find(loc => loc.id === employee.locationId)
-            
-            return <Employee key={employee.id} employee={employee} location={clinic} />})}
+
+        <article className="employees">
+            {
+                employees.map(employee => {
+                    return <Link key={employee.id} to={`/employees/${employee.id}`}>
+                        <h3>{employee.name}</h3>
+                    </Link>
+                })
+            }
         </article>
-            
     </div>
 )
 }
